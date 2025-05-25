6dc9f905ec6912bb0cd6ce06daec1d947f03ce0e
maja <maya-kut@mail.ru>
2025-05-25 17:34:32 +0300

    first test add

--- src/test/java/SelenideFilesTest.java
+++ src/test/java/SelenideFilesTest.java
@@ -1,8 +1,25 @@
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class SelenideFilesTest {
-    @Test
-    void downloadFileTest(){
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
+public class SelenideFilesTest {
+    private final ClassLoader cl = SelenideFilesTest.class.getClassLoader();
+
+    @Test
+    @DisplayName("Проверка наличия файлов в архиве")
+    public void zipFileParsingTest() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                Objects.requireNonNull(cl.getResourceAsStream("Archive.zip"))
+        )) {
+            ZipEntry entry;
+
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println(entry.getName());
+            }
+        }
     }
 }
