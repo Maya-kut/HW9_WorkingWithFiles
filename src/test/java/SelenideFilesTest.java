0a12e3857d45465144925fee81a6cea4edf5da48
maja <maya-kut@mail.ru>
2025-05-25 19:01:58 +0300

    second test add

--- src/test/java/SelenideFilesTest.java
+++ src/test/java/SelenideFilesTest.java
@@ -1,7 +1,11 @@
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
 import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -19,6 +23,45 @@ public class SelenideFilesTest {
 
             while ((entry = zis.getNextEntry()) != null) {
                 System.out.println(entry.getName());
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Проверка  pdf файла")
+    public void pdfFileParsingTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("Archive.zip")) {
+            assert is != null;
+            try (ZipInputStream zis = new ZipInputStream(is)) {
+                ZipEntry zipEntry;
+                while ((zipEntry = zis.getNextEntry()) != null) {
+                    if (zipEntry.getName().endsWith(".pdf")) {
+                        PDF pdf = new PDF(zis);
+                        Assertions.assertEquals("Тестовый PDF-документ\n" +
+                                "Здравствуйте!\n" +
+                                "Это документ в формате PDF, который был создан для тестирования загрузки файлов.\n" +
+                                "Никакой полезной информации он не несёт.",pdf.text);
+                    }
+                }
+            }
+        }
+    }
+    @Test
+    @DisplayName("Проверка xls файла")
+    public void xlsFileParsingTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("Archive.zip")) {
+            assert is != null;
+            try (ZipInputStream zis = new ZipInputStream(is)) {
+                ZipEntry zipEntry;
+                while ((zipEntry = zis.getNextEntry()) != null) {
+                    if (zipEntry.getName().endsWith(".pdf")) {
+                        XLS xls = new XLS(zis);
+                        Assertions.assertEquals("Тестовый PDF-документ\n" +
+                                "Здравствуйте!\n" +
+                                "Это документ в формате PDF, который был создан для тестирования загрузки файлов.\n" +
+                                "Никакой полезной информации он не несёт.",pdf.text);
+                    }
+                }
             }
         }
     }
