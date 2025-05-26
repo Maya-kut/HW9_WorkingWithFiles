22fe49fcfbf8c2ab7bb80d6fcf94bf7365b76c7d
maja <maya-kut@mail.ru>
2025-05-26 21:16:53 +0300

    test add

--- src/test/java/SelenideFilesTest.java
+++ src/test/java/SelenideFilesTest.java
@@ -1,11 +1,14 @@
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
 import java.util.Objects;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -40,12 +43,13 @@ public class SelenideFilesTest {
                         Assertions.assertEquals("Тестовый PDF-документ\n" +
                                 "Здравствуйте!\n" +
                                 "Это документ в формате PDF, который был создан для тестирования загрузки файлов.\n" +
-                                "Никакой полезной информации он не несёт.",pdf.text);
+                                "Никакой полезной информации он не несёт.", pdf.text);
                     }
                 }
             }
         }
     }
+
     @Test
     @DisplayName("Проверка xls файла")
     public void xlsFileParsingTest() throws Exception {
@@ -56,10 +60,38 @@ public class SelenideFilesTest {
                 while ((zipEntry = zis.getNextEntry()) != null) {
                     if (zipEntry.getName().endsWith(".xls")) {
                         XLS xls = new XLS(zis);
-                        String firstString = xls.excel.getSheetAt(0).getRow(1).getCell(1).getStringCellValue();
-                        String secondString = xls.excel.getSheetAt(0).getRow(3).getCell(4).getStringCellValue();
-                        Assertions.assertEquals("Проверка строки в xl файле", firstString);
-                        Assertions.assertEquals("проверка ещё одной строки в xl файле", secondString);
+                        String firstString = xls.excel
+                                .getSheetAt(0)
+                                .getRow(1)
+                                .getCell(1)
+                                .getStringCellValue();
+                        String secondString = xls.excel
+                                .getSheetAt(0)
+                                .getRow(2)
+                                .getCell(2)
+                                .getStringCellValue();
+                        Assertions.assertEquals("Внешний", firstString);
+                        Assertions.assertEquals("Иванова", secondString);
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("Проверка csv файла")
+    public void csvFileParsingTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("Archive.zip")) {
+            assert is != null;
+            try (ZipInputStream zis = new ZipInputStream(is)) {
+                ZipEntry zipEntry;
+                while ((zipEntry = zis.getNextEntry()) != null) {
+                    if (zipEntry.getName().endsWith(".csv")) {
+                        CSVReader reader = new CSVReader(';', new InputStreamReader(zis));
+                        List<String[]> data = reader.readAll();
+                        Assertions.assertEquals(2, data.size());
+                        Assertions.assertArrayEquals(new String[]{"CN001;OU001;iivanova@company.ru;88002000600;131;iivanova;iivanova"}, data.get(0));
+                        Assertions.assertArrayEquals(new String[]{"CN002;OU002;ksergeev@company.ru;;201;ksergeev;ksergeev"}, data.get(1));
                     }
                 }
             }
